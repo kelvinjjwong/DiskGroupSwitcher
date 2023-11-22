1b708d2b34e7f904c1f050129ff4cac9c28d00f3
kelvinjjwong <kelvinjjwong@outlook.com>
2023-11-23 07:52:15 +0800

    Initial Commit

---
+++ DiskGroupSwitcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DiskGroupSwitcher
+//
+//  Created by kelvinwong on 2023/11/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
